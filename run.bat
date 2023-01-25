62f2e792b5ea8c1913734238b90532c576777c48
MaiDucHuong <maiduchuong2811@gmail.com>
2023-01-25 17:21:52 +0700

    nro

---
+++ run.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+java -server -jar -Dfile.encoding=UTF-8 -Xms1000M -Xmx1000M \target\DragonBoy-1.0-SNAPSHOT.jar
+
+PAUSE
---
+++ src/main/java/nro/constant/Constant.java
@@ -0,0 +1,11 @@
+package nro.constant;
+
+public class Constant {
+    public static final long MAX_MONEY = 2000000000L;
+    public static final int MAX_GEM = (int) 2000000000L;
+    public static final int MAX_RUBY = (int) 2000000000L;
+
+    public static final int MAX_TOP_POWER = 10;
+
+    public static final int MAX_TOP_CARD = 10;
+}
